d781a0fedc4fd8d396bd4ba4f32abbf602e3b6ec
Gene Kogan <kogan.gene@gmail.com>
2023-02-09 14:15:07 -0800

    upload file example

---
+++ examples/upload_files.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+
+EDEN_API_URL = "https://api.eden.art"
+
+header = {
+    "x-api-key": "YOUR_API_KEY",
+    "x-api-secret": "YOUR_API_SECRET"
+}
+
+def upload_file(file_path):
+    with open(file_path, "rb") as file:
+        media = file.read()
+        filename = os.path.basename(file_path)
+        files = {"media": (filename, media)}
+        response = requests.post(EDEN_API_URL + "/media/upload", files=files, headers=header)
+        return response.json()
+
+
+result = upload_file("/Users/genekogan/eden/eden-sdk/examples/assets/test.jpg")
+print(result)
